1988bff3e1bbbf8a6c08f85ec51887ed37ff84cd
Neto322 <42181276+Neto322@users.noreply.github.com>
2019-02-25 01:14:31 -0600

    Trasladado de google drive

---
+++ Assets/MonitoBienLoco.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitoBienLoco : MonoBehaviour {
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    public float velocida;
+    public float salto;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+   
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	if(Input.GetKey(KeyCode.A))
+        {
+            sprite.flipX = true;
+            rb.AddForce(-transform.right * velocida);
+        }
+   
+    	if(Input.GetKey(KeyCode.D))
+        {
+            sprite.flipX = false;
+           
+            rb.AddForce(transform.right* velocida);
+        }
+	
+	if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.up* salto, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.up * velocida);
+        }
+    }
+    	
+	}
+     
+
